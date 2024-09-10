3f9b68c3c63ef86bc09a0c81bea6e2e5fa1ea885
akshaydhayal <akshaydhayalkd99@gmail.com>
2024-09-10 08:57:17 +0530

    Added the actions routes

---
+++ web/app/api/action/assetMetadata.ts
@@ -0,0 +1,31 @@
+export const assetMetadataUri = {
+  solana: [
+    'https://arweave.net/zzXHoRWMMN09H8KiAK3aUV_FEfhl5uy_pKHiKjHp1vo',
+    'https://arweave.net/bnwiequyxiL8wFkce8EbV5NhiOTz1Flb6Biyw7pcLB0',
+    'https://arweave.net/jHxiIaUm1J982J7gRNmXP6BnWqK1feW3Ou0-vUvApZU',
+    'https://arweave.net/LLNB-9iVVi5Ty3fkyiOHz5gBCuRiL2JrInx1LaDTRUQ',
+  ],
+  metaplex: [
+    'https://arweave.net/dYPYFZL8jmHwrUhWaJADI0pRwUjYOr1Id17YCxxYTJM',
+    'https://arweave.net/mxDLe7RUyOndi7bKnRGs22cU6DXoP7BjyFolS5bNvsE',
+    'https://arweave.net/Np3jW1JWqIGwyRqI1ZVcW5J-m9TYHwnunRFGcW-qSuo',
+  ],
+  blockchain: [
+    'https://arweave.net/DDDCkPrNv7WfB5WU9QoOVTaubsRP3jwJnxY8wePMGn4',
+    'https://arweave.net/wJv0GRmxDWLEp7U2Gvz6mSmb9BoNYctB_E07cA2-c4I',
+    'https://arweave.net/9oBZfEb0XAx2o-sGoWEakEv1BA1d9ANj0WbPDr3VfqI',
+  ],
+};
+
+// metaplex1 uri1 :  https://arweave.net/dYPYFZL8jmHwrUhWaJADI0pRwUjYOr1Id17YCxxYTJM
+// metaplex1 uri2 :  https://arweave.net/mxDLe7RUyOndi7bKnRGs22cU6DXoP7BjyFolS5bNvsE
+// metaplex1 uri3 :  https://arweave.net/Np3jW1JWqIGwyRqI1ZVcW5J-m9TYHwnunRFGcW-qSuo
+
+// blockchain uri1 :  https://arweave.net/DDDCkPrNv7WfB5WU9QoOVTaubsRP3jwJnxY8wePMGn4
+// blockchain uri2 :  https://arweave.net/wJv0GRmxDWLEp7U2Gvz6mSmb9BoNYctB_E07cA2-c4I
+// blockchain uri3 :  https://arweave.net/9oBZfEb0XAx2o-sGoWEakEv1BA1d9ANj0WbPDr3VfqI
+
+// solana uri1 :  https://arweave.net/zzXHoRWMMN09H8KiAK3aUV_FEfhl5uy_pKHiKjHp1vo
+// solana uri2 :  https://arweave.net/bnwiequyxiL8wFkce8EbV5NhiOTz1Flb6Biyw7pcLB0
+// solana uri3 :  https://arweave.net/jHxiIaUm1J982J7gRNmXP6BnWqK1feW3Ou0-vUvApZU
+// solana uri4 :  https://arweave.net/LLNB-9iVVi5Ty3fkyiOHz5gBCuRiL2JrInx1LaDTRUQ
--- web/app/api/action/route.ts
+++ web/app/api/action/route.ts
@@ -1,3 +1,138 @@
+import {
+  ActionGetResponse,
+  ActionPostResponse,
+  ACTIONS_CORS_HEADERS,
+} from '@solana/actions';
+import {
+  clusterApiUrl,
+  Connection,
+  PublicKey,
+  Transaction,
+} from '@solana/web3.js';
+// import { closeEmptyAccounts } from './helper';
+import {
+  createCloseAccountInstruction,
+  TOKEN_2022_PROGRAM_ID,
+  TOKEN_PROGRAM_ID,
+} from '@solana/spl-token';
+
 export async function GET(request: Request) {
-  return new Response('Hello, from API!');
+  const response: ActionGetResponse = {
+    title: 'Play Quiz and Earn a NFT',
+    icon: 'https://s3.coinmarketcap.com/static-gravity/image/5cc0b99a8dd84fbfa4e150d84b5531f2.png',
+    // icon: 'http://localhost:3001/fee3.png',
+    // icon: new URL(request.url).origin + '/fee5.png',
+    description: `
+    Q-1) What is the base transaction fee on Solana?
+      a) 1,000 lamports   b) 5,000 lamports  c) 10,000 lamports
+
+    Q-2) What is the maximum size of a Solana transaction?
+      a) 1232 bytes  b) 1024 bytes  c) 1500 bytes
+
+    Q-3) What happens if an instruction within a transaction fails?
+      a) Only the failing instruction is rolled back,
+      b) The transaction continues with the remaining instructions,
+      c) The entire transaction fails`,
+
+    label: 'BLINK',
+    links: {
+      actions: [
+        // {
+        //   label: 'Option ABC',
+        //   href: request.url + '?pg=token',
+        // },
+        // {
+        //   label: 'Option ABC',
+        //   href: request.url + '?pg=token',
+        // },
+        // {
+        //   label: 'Option ABC',
+        //   href: request.url + '?pg=token',
+        // },
+        // {
+        //   label: 'Option ABC',
+        //   href: request.url + '?pg=token',
+        // },
+        // {
+        //   label: 'Option ABC',
+        //   href: request.url + '?pg=token',
+        // },
+        // {
+        //   label: 'Option ABC',
+        //   href: request.url + '?pg=token',
+        // },
+        // {
+        //   label: 'Option ABC',
+        //   href: request.url + '?pg=token',
+        // },
+
+        // {
+        //   label: 'Close Token Program Accounts',
+        //   href: request.url + '?pg=token',
+        //   parameters: [
+        //     {
+        //       name: 'ans',
+        //       label: 'Answer',
+        //     },
+        //   ],
+        // },
+
+        {
+          label: 'Submit Quiz',
+          href: request.url + '?pg=token',
+          parameters: [
+            {
+              type: 'select',
+              name: 'Correct Answer',
+              label: 'select Correct Options',
+              options: [
+                { label: 'Option1 - aba', value: 'ABC', selected: true },
+                { label: 'Option2 - abb', value: 'ABC', selected: true },
+                { label: 'Option3 - abc', value: 'ABC', selected: true },
+                { label: 'Option4 - bca', value: 'ABC', selected: true },
+                { label: 'Option5 - bba', value: 'ABC', selected: true },
+                { label: 'Option6 - bac', value: 'ABC', selected: true },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+  };
+  return Response.json(response, { headers: ACTIONS_CORS_HEADERS });
+}
+
+export const OPTIONS = GET;
+
+export async function POST(request: Request) {
+  const connection = new Connection(clusterApiUrl('devnet'));
+  const reqBody = await request.json();
+  let sender = new PublicKey(reqBody.account);
+
+  const pg = new URL(request.url).searchParams.get('pg');
+  const programId = pg == 'token' ? TOKEN_PROGRAM_ID : TOKEN_2022_PROGRAM_ID;
+  // const emptyTAs = await closeEmptyAccounts(connection, sender, programId);
+
+  const tx = new Transaction();
+  tx.feePayer = sender;
+  const blockHeight = await connection.getLatestBlockhash();
+  tx.recentBlockhash = blockHeight.blockhash;
+  tx.lastValidBlockHeight = blockHeight.lastValidBlockHeight;
+  // const ixs = emptyTAs.map((acc) =>
+  //   createCloseAccountInstruction(acc, sender, sender, undefined, programId)
+  // );
+
+  // if (ixs.length > 0) {
+  //   tx.add(...ixs);
+  // }
+  const serialisedTx = tx
+    .serialize({ requireAllSignatures: false, verifySignatures: false })
+    .toString('base64');
+
+  const response: ActionPostResponse = {
+    transaction: serialisedTx,
+    // transaction:"",
+    message: 'Wooh!!, Closed all Empty Token Accounts',
+  };
+  return Response.json(response, { headers: ACTIONS_CORS_HEADERS });
 }
